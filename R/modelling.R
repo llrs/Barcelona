4a423ab40ffb5598e4bab42bf2ed5b374617fa22
llrs <lluis.revilla@gmail.com>
2019-01-09 17:31:32 +0100

    Introduce the other designs

--- R/modelling.R
+++ R/modelling.R
@@ -4,25 +4,13 @@ library("RGCCA2")
 A <- readRDS("data/RGCCA_data.RDS")
 A2 <- A[1:2]
 
-# The design
+# Basic ####
 C <- matrix(
   0, ncol = length(A2), nrow = length(A2),
   dimnames = list(names(A2), names(A2))
 )
 model0 <- subSymm(C, "Micro", "RNAseq", 1)
 model0i <- subSymm(model0, 1, 1, 1)
-
-C <- matrix(
-  0, ncol = length(A), nrow = length(A),
-  dimnames = list(names(A), names(A))
-)
-model1 <- subSymm(C, "Micro", "Meta", 1)
-model1 <- subSymm(model1, "RNAseq", "Meta", 1)
-model1i <- subSymm(model1, 1, 1, 1)
-model2 <- subSymm(model1, "RNAseq", "Micro", 1)
-model2i <- subSymm(model2, 1, 1, 1)
-
-
 
 # We cannnot comput eht tau.estimate for A[[1]]
 # (shrinkage <- sapply(A, tau.estimate))
@@ -79,15 +67,26 @@ comm +
   geom_text(aes(color = A$Meta$IBD.x, label = A$Meta$Original)) +
   guides(col = guide_legend(title = "Patient"))
 
+# With metadata ####
+C <- matrix(
+  0, ncol = length(A), nrow = length(A),
+  dimnames = list(names(A), names(A))
+)
+model1 <- subSymm(C, "Micro", "Meta", 1)
+model1 <- subSymm(model1, "RNAseq", "Meta", 1)
+model1i <- subSymm(model1, 1, 1, 1)
+model2 <- subSymm(model1, "RNAseq", "Micro", 1)
+model2i <- subSymm(model2, 1, 1, 1)
+
 models2 <- list(model1, model1i, model2, model2i)
+names(models2) <- c("model1", "model1i", "model2", "model2i")
 A2 <- A
-A2$meta <- model_RGCCA(A$Meta, c("ID", "AgeDiag", "diagTime", "Exact_location", "SEX"))
+A2$Meta <- model_RGCCA(A$Meta, c("ID", "AgeDiag", "diagTime", "Exact_location", "SEX"))
 A2b <- lapply(A2, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
-out <- lapply(models2, use, A = Ab2, c1 = shrinkage[1:2])
+out <- lapply(models2, use, A = Ab2, c1 = shrinkage)
+saveRDS(out, "models2.RDS")
 
-
-
-
+# Complex models ####
 Localization <- model_RGCCA(A$Meta, c("Exact_location")) # With SESCD local it increase the AVE_inner
 Time <- model_RGCCA(A$Meta, c("AgeDiag", "Age"))
 Demographics <- model_RGCCA(A$Meta, c("ID","SEX"))
@@ -96,3 +95,30 @@ A3 <- A[1:2]
 A3$Demographics <- Demographics
 A3$Localization <- Localization
 A3$Time <- Time
+A3b <- lapply(A3, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
+
+C <- matrix(
+  0, ncol = length(A3), nrow = length(A3),
+  dimnames = list(names(A3), names(A3))
+)
+model3 <- subSymm(C, "Micro", "RNAseq", 1)
+model3 <- subSymm(model3, "RNAseq", "Demographics", 1)
+model3 <- subSymm(model3, "RNAseq", "Localization", 1)
+model3 <- subSymm(model3, "RNAseq", "Time", 1)
+model3 <- subSymm(model3, "Micro", "Demographics", 1)
+model3 <- subSymm(model3, "Micro", "Localization", 1)
+model3 <- subSymm(model3, "Micro", "Time", 1)
+model3i <- subSymm(model3, 1, 1, 1)
+
+model3b <- subSymm(C, "RNAseq", "Localization", 1)
+model3b <- subSymm(model3b, "Demographics", "Micro", 1)
+model3b <- subSymm(model3b, "Localization", "Micro", 0.5)
+model3b <- subSymm(model3b, "Demographics", "Time", 1)
+model3bi <- subSymm(model3b, 1, 1, 1)
+
+models3 <- list(model3, model3i, model3b, model3bi)
+shrinkage <- rep(1, length(A3b))
+shrinkage[1:2] <- shrinkage[1:2]
+names(models3) <- c("model3", "model3i", "model3b", "model3bi")
+out <- lapply(models3, use, A = A3b, c1 = shrinkage)
+saveRDS(out, "models3.RDS")
