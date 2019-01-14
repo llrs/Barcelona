fdc6e39dab91f87ec1e44aa70f1bc40c8e75a134
llrs <lluis.revilla@gmail.com>
2019-01-14 17:48:48 +0100

    Look for the best model3

---
+++ R/models3.R
@@ -0,0 +1,88 @@
+library("integration")
+library("RGCCA2")
+library("dplyr")
+
+A <- readRDS("data/RGCCA_data.RDS")
+meta <- A$Meta
+
+testing <- function(x, ...) {
+  tryCatch({
+  result.sgcca <- RGCCA2::sgcca(C = x,
+                                scheme = "centroid",
+                                verbose = FALSE,
+                                scale = FALSE,
+                                ...)
+  analyze(result.sgcca)}
+  , error = function(x){NA})
+}
+
+Localization <- model_RGCCA(A$Meta, c("Exact_location")) # With SESCD local it increase the AVE_inner
+Time <- model_RGCCA(A$Meta, c("AgeDiag", "Age"))
+Demographics <- model_RGCCA(A$Meta, c("ID","SEX"))
+Time$AgeDiag[is.na(Time$AgeDiag)] <- 0 # Time has NA values
+A2 <- A[1:2]
+A2$Demographics <- Demographics
+A2$Location <- Localization
+A2$Time <- Time
+
+A2 <- clean_unvariable(A2)
+saveRDS(A2, "model3_BCN.RDS")
+
+shrinkage <- sapply(A2[1:2], tau.estimate) # 0.11503779803812 0.318145965316924
+shrinkage[3:5] <- 1
+names(shrinkage) <- names(A2)
+Ab <- lapply(A2, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
+
+
+
+# The design of model 3
+C <- matrix(
+  0, ncol = length(Ab), nrow = length(Ab),
+  dimnames = list(names(Ab), names(Ab))
+)
+
+designs <- weight_design(weights = 3, size = length(Ab))
+keep <- vapply(designs, correct, logical(1L))
+designs <- designs[keep]
+
+# Subset the designs
+set.seed(46726279)
+s <- sample(designs, size = min(length(designs)*.1, 1000))
+
+out <- sapply(s, testing, A = Ab, c1 = shrinkage, USE.NAMES = FALSE)
+out2 <- out[lengths(out) == 24]
+out2 <- simplify2array(out2)
+out2 <- as.data.frame(t(out2))
+saveRDS(out2, "sample_model3_boot.RDS")
+
+out %>%
+  top_n(5, AVE_inner) %>%
+  select(AVE_inner, AVE_outer, var12, var13, var23,
+         var14, var24, var34, var15, var25, var35, var45) %>%
+  arrange(desc(AVE_inner))
+stop("Visual inspection of the top 5")
+
+s2 <- sample(designs, size = min(length(designs)*.1, 1000))
+
+out <- sapply(s2, testing, A = Ab, c1 = shrinkage, USE.NAMES = FALSE)
+out2 <- out[lengths(out) == 24]
+out2 <- simplify2array(out2)
+out2 <- as.data.frame(t(out2))
+saveRDS(out2, "sample2_model3_boot.RDS")
+stop("Visual inspection of the top 5")
+
+out1 <- readRDS("sample_model3_boot.RDS")
+out0 <- rbind(out1, out2)
+out <- out0[!duplicated(out0), ]
+ggplot(out, aes(AVE_inner, AVE_outer)) +
+  geom_point()
+
+keep_best <- vapply(designs, function(x){
+  x[1, 2] == 0 & x[2, 3] == 0
+}, logical(1L))
+
+out <- sapply(designs[keep_best], testing, A = Ab, c1 = shrinkage, USE.NAMES = FALSE)
+out2 <- out[lengths(out) == 24]
+out2 <- simplify2array(out2)
+out2 <- as.data.frame(t(out2))
+saveRDS(out2, "sample_def_model3_boot.RDS")
