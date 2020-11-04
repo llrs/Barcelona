88612996dfa76d066ff20e18261e9b4af4c1954d
llrs <lluis.revilla@gmail.com>
2020-11-04 11:18:09 +0100

    Prepare for ASV

---
+++ R/ASV_preparation.R
@@ -0,0 +1,26 @@
+library("R.utils")
+
+# Read files to copy ####
+meta <- readRDS("data_out/refined_meta.RDS")
+samples <- meta$Original
+stopifnot(length(samples) == 128)
+
+# Copy from the disk to the computer ####
+out <- lapply(samples, function(x) {
+  loc <- list.files(path = "/media/lrevilla/Elements/SEQ/Michigan/Complete/",
+                    recursive = TRUE, full.names = TRUE,
+                    pattern = paste0("^", x, ".*.fastq.gz"))
+  dest <- "/home/lrevilla/Documents/projects/design_ngs/data/fastq_ASV/"
+  file.copy(from = loc, to = dest, copy.date = TRUE, overwrite = FALSE)
+})
+# Remove some files I souldn't copy ####
+dest <- "/home/lrevilla/Documents/projects/design_ngs/data/fastq_ASV"
+keep <- lapply(samples, function(x){
+  list.files(path = dest, full.names = TRUE, pattern = x)})
+keep <- unlist(keep)
+all_files <- list.files(path = dest, full.names = TRUE)
+remove_files <- all_files[!all_files %in% keep]
+file.remove(remove_files)
+
+# Uncompress files ####
+f <- lapply(keep, gunzip)
