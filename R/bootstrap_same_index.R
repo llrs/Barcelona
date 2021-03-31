d1a800f1ca3cf692f0c9b9fe42f445d06092f693
llrs <lluis.revilla@gmail.com>
2021-03-31 16:55:21 +0200

    Uncomment code

--- R/bootstrap_same_index.R
+++ R/bootstrap_same_index.R
@@ -12,34 +12,35 @@ set.seed(9876156)
 # index <- inteRmodel::boot_index(nrow(A[[1]]), boots)
 # saveRDS(index, file = "data_out/index_boot2.RDS")
 index <- readRDS("data_out/index_boot2.RDS")
-#
+
 # * Model 0 ####
-# shrinkage <- c(0.322297910454825, 0.866155549496009) #Calculated from the server for the data derived from original data
-# shrinkage[2] <- tau.estimate(A[[2]])
-# Ab <- lapply(A[1:2], function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
-# names(Ab) <- names(A[1:2])
-# ab <- clean_unvariable(Ab)
-# mcp <- MulticoreParam(workers = 8, progressbar = TRUE)
-# b0 <- boot_index_sgcca(index, A = ab, c1 = shrinkage, scheme = "centroid",
-#                          scale = FALSE, verbose = FALSE, bias = TRUE,
-#                        BPPARAM = mcp)
-# saveRDS(b0, "data_out/boot_0_b.RDS")
-#
-# # * Model 1.2 ####
-# model2_best <- readRDS("data_out/model2b2_sgcca_b.RDS")
-# C1.2 <- model2_best$C
-#
-# A$Meta <- model_RGCCA(A$Meta, c("ID", "AgeDiag", "diagTime", "Exact_location", "SEX"))
-# Ab <- lapply(A, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
-# Ab <- clean_unvariable(Ab)
-# shrinkage <- rep(1, 3)
-# shrinkage[1:2] <- c(0.322020648273615, 0.866155549496009)
-#
-#
-# b1.2 <- boot_index_sgcca(index, A = Ab, c1 = shrinkage, scheme = "centroid",
-#                           scale = FALSE, verbose = FALSE, bias = TRUE, C = C1.2,
-#                          BPPARAM = mcp)
-# saveRDS(b1.2, "data_out/boot_1.2_b.RDS")
+shrinkage <- c(0.322297910454825, 0.866155549496009) #Calculated from the server for the data derived from original data
+shrinkage[2] <- tau.estimate(A[[2]])
+Ab <- lapply(A[1:2], function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
+names(Ab) <- names(A[1:2])
+ab <- clean_unvariable(Ab)
+C0 <- matrix(c(0, 1, 1, 0), nrow = 2)
+mcp <- MulticoreParam(workers = 8, progressbar = TRUE)
+b0 <- boot_index_sgcca(index, A = ab, c1 = shrinkage, scheme = "centroid",
+                         scale = FALSE, verbose = FALSE, bias = TRUE, C = C0,
+                       BPPARAM = mcp)
+saveRDS(b0, "data_out/boot_0_b.RDS")
+
+# * Model 1.2 ####
+model2_best <- readRDS("data_out/model2b2_sgcca_b.RDS")
+C1.2 <- model2_best$C
+
+A$Meta <- model_RGCCA(A$Meta, c("ID", "AgeDiag", "diagTime", "Exact_location", "SEX"))
+Ab <- lapply(A, function(x) scale2(x, bias = TRUE)/sqrt(NCOL(x)))
+Ab <- clean_unvariable(Ab)
+shrinkage <- rep(1, 3)
+shrinkage[1:2] <- c(0.322020648273615, 0.866155549496009)
+
+
+b1.2 <- boot_index_sgcca(index, A = Ab, c1 = shrinkage, scheme = "centroid",
+                          scale = FALSE, verbose = FALSE, bias = TRUE, C = C1.2,
+                         BPPARAM = mcp)
+saveRDS(b1.2, "data_out/boot_1.2_b.RDS")
 
 # * Model 2.2 ####
 model3_best <- readRDS("data_out/model3_best_treatment_b.RDS")
